107d3ba83cbbec9c2afab9ce5e67497fa6611446
Aaron <arnhsmith@gmail.com>
2023-09-06 20:04:21 -0400

    validations added

--- server/models.py
+++ server/models.py
@@ -80,29 +80,29 @@ class Drink(db.Model, SerializerMixin):
 
 
     # validations
-    # @validates('name')
-    # def validate_name(self, key, name):
-    #     if not name or name.length() <= 0:
-    #         raise ValueError('Invalid name provided')
-    #     return name
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name or name.length() <= 0:
+            raise ValueError('Invalid name provided')
+        return name
 
-    # @validates('description')
-    # def validate_drink_description(self, key, drink_description):
-    #     if not drink_description or drink_description.length() <= 0:
-    #         raise ValueError('Invalid drink description provided')
-    #     return drink_description
+    @validates('description')
+    def validate_drink_description(self, key, drink_description):
+        if not drink_description or drink_description.length() <= 0:
+            raise ValueError('Invalid drink description provided')
+        return drink_description
 
-    # @validates('price')
-    # def validate_price(self, key, price):
-    #     if not price or price < 0:
-    #         raise ValueError('Invalid price provided')
-    #     return price
+    @validates('price')
+    def validate_price(self, key, price):
+        if not price or price < 0:
+            raise ValueError('Invalid price provided')
+        return price
 
-    # @validates('ingredients_needed')
-    # def validate_price(self, key, ingredients_needed):
-    #     if not ingredients_needed or ingredients_needed < 0:
-    #         raise ValueError('Invalid ingredients needed provided')
-    #     return ingredients_needed
+    @validates('ingredients_needed')
+    def validate_price(self, key, ingredients_needed):
+        if not ingredients_needed or ingredients_needed < 0:
+            raise ValueError('Invalid ingredients needed provided')
+        return ingredients_needed
 
     def __repr__(self):
         return f'<Drink {self.id}>'
@@ -113,8 +113,6 @@ class Ingredient(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
-    # user_id = db.Column(db.Integer)
 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
@@ -128,17 +126,17 @@ class Ingredient(db.Model, SerializerMixin):
 
 
     # validations
-    # @validates('name')
-    # def validate_name(self, key, name):
-    #     if not name or len(name) <= 0:
-    #         raise ValueError('Invalid name provided')
-    #     return name
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name or len(name) <= 0:
+            raise ValueError('Invalid name provided')
+        return name
 
-    # @validates('ingredient_description')
-    # def validate_ingredient_discription(self, key, ingredient_description):
-    #     if not ingredient_description or ingredient_description.length() <= 0:
-    #         raise ValueError('Invalid ingredient description provided')
-    #     return ingredient_description
+    @validates('ingredient_description')
+    def validate_ingredient_discription(self, key, ingredient_description):
+        if not ingredient_description or ingredient_description.length() <= 0:
+            raise ValueError('Invalid ingredient description provided')
+        return ingredient_description
 
     def __repr__(self):
         return f'<Ingredient {self.id}>'
